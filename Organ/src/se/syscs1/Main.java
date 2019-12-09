2a6bd7f6c001d4ea31fdf5ffbaa34d144a8c4e30
sighBRG <55457027+sighBRG@users.noreply.github.com>
2019-12-09 13:18:20 +0100

    Update Main.java

--- Organ/src/se/syscs1/Main.java
+++ Organ/src/se/syscs1/Main.java
@@ -11,4 +11,10 @@ package se.syscs1;
  */
 public class Main {
     
+    public static void main(String[] args) {
+        System.out.println("HEJ");
+        int myVar = 123;
+        System.out.println(myVar);
+    }
+    
 }
